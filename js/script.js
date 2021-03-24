097c0264918dfaa564ca51ac95ad8765d6106531
Isaac Matos <isaachavester@gmail.com>
2021-03-24 20:51:40 -0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function clicou_g(){
+  alert("Clique em OK para abrir em uma nova aba o meu perfil do GitHub!");
+}
+
+function clicou_l(){
+  alert("Clique em OK para abrir em uma nova aba o meu perfil do LinkedIn!");
+}
+
+function clicou_t(){
+  alert("Clique em ok para abrir em uma nova aba o meu perfil do Twitter!");
+}
